36da978c0c093ff668dd5ae55ba372f1467ab6bb
ConradGuillaume <conrad.guillaume0501@gmail.com>
2023-04-05 11:50:16 +0200

    fix

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { AnimatePresence, motion, useAnimation } from "framer-motion";
 import logo from "../style/images/Logo/logo.png";
@@ -9,49 +8,27 @@ const MobileNav = () => {
   const animStick1 = useAnimation();
   const animStick2 = useAnimation();
   const animStick3 = useAnimation();
+
+  const handleBurgerClick = () => {
+    animStick1.start({
+      rotate: burger ? 0 : 45,
+      scale: burger ? 1 : 1.3,
+      translateX: 0,
+      translateY: burger ? 0 : 15,
+    });
+    animStick2.start({ opacity: burger ? 1 : 0 });
+    animStick3.start({
+      rotate: burger ? 0 : -45,
+      scale: burger ? 1 : 1.3,
+      translateX: 0,
+      translateY: burger ? 0 : -15,
+    });
+    SetBurger(!burger);
+  };
+
   return (
     <div className="menu">
-      <div
-        onClick={() => {
-          if (burger === true) {
-            animStick1.start({
-              rotate: 0,
-              scale: 1,
-              translateX: 0,
-              translateY: 0,
-            });
-            animStick2.start({
-              opacity: 1,
-            });
-            animStick3.start({
-              rotate: 0,
-              scale: 1,
-              translateX: 0,
-              translateY: 0,
-            });
-            SetBurger(false);
-          }
-          if (burger === false) {
-            animStick1.start({
-              rotate: 45,
-              scale: 1.3,
-              translateX: 0,
-              translateY: 15,
-            });
-            animStick2.start({
-              opacity: 0,
-            });
-            animStick3.start({
-              rotate: -45,
-              scale: 1.3,
-              translateX: 0,
-              translateY: -15,
-            });
-            SetBurger(true);
-          }
-        }}
-        className="burger"
-      >
+      <div onClick={handleBurgerClick} className="burger">
         <motion.div animate={animStick1} className="stick-1"></motion.div>
         <motion.div animate={animStick2} className="stick-2"></motion.div>
         <motion.div animate={animStick3} className="stick-3"></motion.div>
@@ -59,12 +36,12 @@ const MobileNav = () => {
       <AnimatePresence>
         {burger && (
           <motion.nav
-            initial={{ x: "20vw" }}
-            animate={{ x: 0, transition: { duration: 0.5, type: "spring" } }}
+            initial={{ x: "150vw" }}
+            animate={{ x: 0, transition: { duration: 1, type: "spring" } }}
             exit={{
-              x: "20vw",
+              x: "50vw",
               opacity: 0,
-              transition: { duration: 1.2, type: "spring" },
+              transition: { duration: 1, type: "spring" },
             }}
             className="burger-nav"
           >
